49e1aaed77874d33fec3cf87aaf73e91ddf4311e
Kennedy Oliveira Rocha <kennedy.rocha@unimed279.corp>
2019-11-12 18:04:43 -0300

    PUT, DELETE e POST para Usuário

--- src/main/java/com/kennedy/denunciesinop/resourses/UsuarioResource.java
+++ src/main/java/com/kennedy/denunciesinop/resourses/UsuarioResource.java
@@ -1,11 +1,16 @@
 package com.kennedy.denunciesinop.resourses;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 
 import com.kennedy.denunciesinop.domain.Usuario;
 import com.kennedy.denunciesinop.services.UsuarioService;
@@ -23,5 +28,32 @@ public class UsuarioResource {
 		Usuario obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/{id}" ,method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody Usuario obj, @PathVariable Integer id){
+
+		obj.setId(id);
+		obj = service.update(obj);
+
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Usuario obj){
+
+		obj = service.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/kennedy/denunciesinop/services/UsuarioService.java
+++ src/main/java/com/kennedy/denunciesinop/services/UsuarioService.java
@@ -1,12 +1,15 @@
 package com.kennedy.denunciesinop.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.kennedy.denunciesinop.domain.Usuario;
 import com.kennedy.denunciesinop.repositories.UsuarioRepository;
+import com.kennedy.denunciesinop.services.exceptions.DataIntegrityException;
 import com.kennedy.denunciesinop.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,5 +24,42 @@ public class UsuarioService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado id: " + id
 										+ ", tipo: " + Usuario.class.getName()));
+	}
+	
+	public Usuario update(Usuario obj) {
+		
+		Usuario newObj = this.find(obj.getId());
+		this.updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public List<Usuario> findAll(){
+		return repo.findAll();
+	}
+	
+	public void delete (Integer id) {
+
+		this.find(id);
+
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+
+			throw new DataIntegrityException("Não possível excluir pois há entidades relacionadas");
+		}
+	}
+	
+	public Usuario insert(Usuario obj) {
+		
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	
+	private void updateData(Usuario objNew, Usuario obj) {
+		
+		objNew.setEmail(obj.getEmail());
+		objNew.setNome(obj.getNome());
 	}
 }
